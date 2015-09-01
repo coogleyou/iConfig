eff11714f0205d9739c0bebf2505eb45e934298e
rollenholt <renwenchao888@gmail.com>
2015-09-01 22:40:51 +0800

    调整工程结构为父子工程

---
+++ iConfig-server/src/java/ren/wenchao/iconfig/api/ZookeeperController.java
@@ -0,0 +1,14 @@
+package ren.wenchao.iconfig.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by wenchao.ren on 2015/9/1.
+ */
+@Controller
+@RequestMapping(value = "/iconfig/zookeeper")
+public class ZookeeperController {
+
+
+}
