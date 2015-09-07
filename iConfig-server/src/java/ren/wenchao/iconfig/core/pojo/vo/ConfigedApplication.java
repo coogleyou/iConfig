105cfb245c950e795770fc606c17211f20bfd403
rollenholt <renwenchao888@gmail.com>
2015-09-07 10:09:46 +0800

    commit code

--- iConfig-server/src/java/ren/wenchao/iconfig/core/pojo/vo/ConfigedApplication.java
+++ iConfig-server/src/java/ren/wenchao/iconfig/core/pojo/vo/ConfigedApplication.java
@@ -1,7 +1,40 @@
 package ren.wenchao.iconfig.core.pojo.vo;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 /**
  * @author rollenholt
  */
 public class ConfigedApplication {
+
+    /**
+     * 应用编号
+     */
+    private String applicationCode;
+
+    /**
+     * 应用名称
+     */
+    private String applicationName;
+
+    public String getApplicationCode() {
+        return applicationCode;
+    }
+
+    public void setApplicationCode(String applicationCode) {
+        this.applicationCode = applicationCode;
+    }
+
+    public String getApplicationName() {
+        return applicationName;
+    }
+
+    public void setApplicationName(String applicationName) {
+        this.applicationName = applicationName;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
 }
--- iConfig-server/src/java/ren/wenchao/iconfig/core/service/ApplicationService.java
+++ iConfig-server/src/java/ren/wenchao/iconfig/core/service/ApplicationService.java
@@ -2,8 +2,15 @@ package ren.wenchao.iconfig.core.service;
 
 import org.springframework.stereotype.Service;
 import ren.wenchao.iconfig.common.zookeeper.ZkComponent;
+import ren.wenchao.iconfig.core.dao.ApplicationDao;
+import ren.wenchao.iconfig.core.pojo.vo.ApplicationConfigInfo;
+import ren.wenchao.iconfig.core.pojo.vo.ConfigedApplication;
 
 import javax.annotation.Resource;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Strings.isNullOrEmpty;
 
 /**
  * @author rollenholt
@@ -13,4 +20,24 @@ public class ApplicationService {
 
     @Resource
     private ZkComponent zkComponent;
+
+    @Resource
+    private ApplicationDao applicationDao;
+
+    /**
+     * 根据用户名查询这个用户所拥有权限的接入iconfig的应用列表
+     *
+     * @param userName 企业内用户的唯一用户名
+     * @return 返回查询到的应用信息
+     */
+    public List<ConfigedApplication> queryConfigedApplications(String userName) {
+        checkArgument(!isNullOrEmpty(userName));
+        return applicationDao.queryConfigedApplications(userName);
+    }
+
+    public ApplicationConfigInfo queryApplicationConfigInfo(String applicationCode) {
+        checkArgument(!isNullOrEmpty(applicationCode));
+        //todo communicate with zk
+        return new ApplicationConfigInfo();
+    }
 }
